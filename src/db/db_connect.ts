2c1a70b4e02a2e4577acc5a294ac46194dfce3e2
dylan <devdylancrowder@outlook.com>
2024-12-17 00:49:08 +0100

    test w

--- src/db/db_connect.ts
+++ src/db/db_connect.ts
@@ -16,20 +16,6 @@ const configTest = {
   queueLimit: 0,
 };
 
-
-/* const configTest = {
-  host: process.env.PROD_DB_HOST,
-  port: 3306,
-  user: process.env.PROD_DB_USER,
-  password: process.env.PROD_DB_PASSWORD,
-  database: process.env.PROD_DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 5,
-  queueLimit: 0,
-}; */
-
-
-
 const configDev = {
   host: "my_mysql",
   port: 3306,
@@ -46,6 +32,18 @@ const configProd = {
   database: process.env.PROD_DB_NAME,
 };
 
+
+
+const git = {
+
+
+  host: "sql10.freemysqlhosting.net",
+  port: 3306,
+  user: "sql10751906",
+  password: "aeutGJMA2C",
+  database: "sql10751906",
+
+}
 const enviroment = process.env.NODE_ENV;
 
 let selectedConfig;
@@ -63,11 +61,11 @@ if (enviroment === "test") {
 // Si no se seleccionó ninguna configuración, puedes hacer un chequeo
 if (!selectedConfig) {
   console.log("No se ha configurado correctamente el entorno de base de datos.");
-  process.exit(1); 
+  process.exit(1);
 }
 
 // Crear el pool de conexiones
-export const connection = mysql.createPool(configProd).promise();
+export const connection = mysql.createPool(git).promise();
 
 // Validar la conexió
 connection
